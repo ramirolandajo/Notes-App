f2c4096b8f6941e3567b854b9938c5cf10673cc5
Ramiro Landajo <ramalandajo@gmail.com>
2023-08-08 12:30:12 -0300

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import icon from "../images/post-it-icon.png"
+export default function Navbar() {
+  return (
+    <nav>
+        <img src={icon} alt="note-it-icon"/>
+    </nav>
+  )
+}
